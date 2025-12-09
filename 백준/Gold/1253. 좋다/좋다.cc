b4d4e57e09d07914bdd186cd7a7604ef556f7c37
KangwookYoon <61686336+theia25@users.noreply.github.com>
2025-12-10 02:42:21 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, result = 0;
+    cin >> N;
+    
+    vector<int> A(N, 0);
+    
+    for(int i = 0; i < N; ++i)
+    {
+        cin >> A[i];
+    }
+    
+    sort(A.begin(), A.end());
+    
+    for(int k = 0; k < N; ++k)
+    {
+        long find = A[k];
+        int i = 0;
+        int j = N - 1;
+        
+        while(i < j)
+        {
+            if(A[i] + A[j] == find)
+            {
+                if(i != k && j != k)
+                {
+                    ++result;
+                    break;
+                }
+                else if(i == k)
+                {
+                    ++i;
+                }
+                else if(j == k)
+                {
+                    --j;
+                }
+            }
+            else if(A[i] + A[j] < find)
+            {
+                ++i;
+            }
+            else
+            {
+                --j;
+            }
+        }
+    }
+    
+    cout << result;
+
+    return 0;
+}
